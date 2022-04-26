9977af3dafae3fa1ce68c376a5282b2f52982f4b
Surv <83309545+SurvExE-Pc@users.noreply.github.com>
2022-04-26 13:38:46 -0400

    uploaded createblobto.js
    It makes it easier to create text blobs off of webpages

---
+++ createblobto.js
@@ -0,0 +1,2 @@
+//Create Blob to - CC SurvExE-Pc github 2019-2022
+function cbto(a){blob=new Blob([a],{type:"text document"});(windowx=window.open("data:text/plain,")).location=URL.createObjectURL(blob)};
